77dc39c6e0f5cac0f4c2b6f4483c1fcf0e23e325
Germey <cqc@cuiqingcai.com>
2020-02-24 06:04:26 +0800

    update deploy

--- proxypool/crawlers/public/ip3366.py
+++ proxypool/crawlers/public/ip3366.py
@@ -6,12 +6,12 @@ import re
 MAX_PAGE = 5
 BASE_URL = 'http://www.ip3366.net/free/?stype=1&page={page}'
 
+
 class IP3366Crawler(BaseCrawler):
     """
     ip3366 crawler, http://www.ip3366.net/
     """
     urls = [BASE_URL.format(page=i) for i in range(1, 8)]
-    
     
     def parse(self, html):
         """
@@ -24,6 +24,7 @@ class IP3366Crawler(BaseCrawler):
         for address, port in re_ip_address:
             proxy = Proxy(host=address.strip(), port=int(port.strip()))
             yield proxy
+
 
 if __name__ == '__main__':
     crawler = IP3366Crawler()
