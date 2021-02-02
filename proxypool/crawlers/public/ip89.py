aa23e0d88c59ecceaf6c5ff25d5ea8b2c4a32825
j.yao.SUSE <everhopingandwaiting@users.noreply.github.com>
2021-02-02 11:51:32 +0800

    Create ip89.py
    www.89ip.cn 免费代理

---
+++ proxypool/crawlers/public/ip89.py
@@ -0,0 +1,34 @@
+from pyquery import PyQuery as pq
+from proxypool.schemas.proxy import Proxy
+from proxypool.crawlers.base import BaseCrawler
+import re
+
+MAX_NUM = 9999
+BASE_URL = 'http://api.89ip.cn/tqdl.html?api=1&num={MAX_NUM}&port=&address=&isp='.format(MAX_NUM=MAX_NUM)
+
+
+class Daili66Crawler(BaseCrawler):
+    """
+    89ip crawler, http://api.89ip.cn
+    """
+    urls = [BASE_URL]
+    
+    def parse(self, html):
+        """
+        parse html file to get proxies
+        :return:
+        """
+        ip_address = re.compile('([\d:\.]*)<br>')
+        hosts_ports = ip_address.findall(html)
+        for addr in hosts_ports:
+            addr_split = addr.split(':')
+            if(len(addr_split) == 2):
+                host = addr_split[0]
+                port = addr_split[1]
+                yield Proxy(host=host, port=port)
+
+
+if __name__ == '__main__':
+    crawler = Daili66Crawler()
+    for proxy in crawler.crawl():
+        print(proxy)
