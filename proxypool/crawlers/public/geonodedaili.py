90975ab009680941cbc3e2f982dc8a14730092ad
Takayama <49364055+MGMCN@users.noreply.github.com>
2023-02-28 15:07:25 +0900

    add headers through crawl function

--- proxypool/crawlers/public/geonodedaili.py
+++ proxypool/crawlers/public/geonodedaili.py
@@ -1,10 +1,9 @@
 import time
-from retrying import RetryError, retry
+from retrying import RetryError
 from loguru import logger
 from proxypool.schemas.proxy import Proxy
 from proxypool.crawlers.base import BaseCrawler
 import json
-import requests
 
 BASE_URL = 'https://proxylist.geonode.com/api/proxy-list?limit=500&page={page}&sort_by=lastChecked&sort_type=desc'
 MAX_PAGE = 18
@@ -35,48 +34,35 @@ class GeonodeCrawler(BaseCrawler):
     def crawl(self):
         """
         override crawl main method
-        modify sleep time .5 to 5.
+        add headers
         """
+        headers = {
+            'authority': 'proxylist.geonode.com',
+            'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"',
+            'accept': 'application/json, text/plain, */*',
+            'sec-ch-ua-mobile': '?0',
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.83 Safari/537.36',
+            'sec-ch-ua-platform': '"macOS"',
+            'origin': 'https://geonode.com',
+            'sec-fetch-site': 'same-site',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-dest': 'empty',
+            'referer': 'https://geonode.com/',
+            'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,ja;q=0.7',
+            'if-none-match': 'W/"c25d-BXjLTmP+/yYXtIz4OEcmdOWSv88"',
+        }
         try:
             for url in self.urls:
                 logger.info(f'fetching {url}')
-                html = self.fetch(url)
+                html = self.fetch(url, headers=headers)
                 if not html:
                     continue
-                time.sleep(5.)
+                time.sleep(.5)
                 yield from self.process(html, url)
         except RetryError:
             logger.error(
                 f'crawler {self} crawled proxy unsuccessfully, '
                 'please check if target url is valid or network issue')
-
-    def fetch(self, url, **kwargs):
-        """
-        override fetch main method
-        add specific headers
-        """
-        try:
-            headers = {
-                'authority': 'proxylist.geonode.com',
-                'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"',
-                'accept': 'application/json, text/plain, */*',
-                'sec-ch-ua-mobile': '?0',
-                'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.83 Safari/537.36',
-                'sec-ch-ua-platform': '"macOS"',
-                'origin': 'https://geonode.com',
-                'sec-fetch-site': 'same-site',
-                'sec-fetch-mode': 'cors',
-                'sec-fetch-dest': 'empty',
-                'referer': 'https://geonode.com/',
-                'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,ja;q=0.7',
-                'if-none-match': 'W/"c25d-BXjLTmP+/yYXtIz4OEcmdOWSv88"',
-            }
-            response = requests.get(url, headers=headers)
-            if response.status_code == 200:
-                response.encoding = 'utf-8'
-                return response.text
-        except (requests.ConnectionError, requests.ReadTimeout):
-            return
 
 
 if __name__ == '__main__':
